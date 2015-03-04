45b99663a68e5d5166204a54cd8ed02e5fb8fcaa
justincoon <justin.tyler.coon@gmail.com>
2015-03-04 14:55:12 -0500

    fixed getting course id in roster

--- course/roster.js
+++ course/roster.js
@@ -1,23 +1,28 @@
 var express     = require('express');
 var router      = express.Router();
 
+//Get course ID from a request
+function getCID(req) {
+    return req.originalUrl.split('/')[2];
+}
+
 //Get the roster of a specific course
 router.get('/', function(req,res) {
-    var cid = req.params.id;
-    console.log(cid + "\n");
+    var cid = getCID(req);
+
 });
 
 //Add a single user to the course's roster
 //NOTE: This will probably have to handle both adding a single user
 //      and adding a file full of users
 router.post('/:id', function(req,res) {
-    var cid = req.params.id;
+    var cid = getCID(req);
 
 });
 
 //Delete a user from a course's roster
-router.delete('/:cid/:uid', function(req,res) {
-    var cid = req.params.cid;
+router.delete('/:uid', function(req,res) {
+    var cid = getCID(req);
     var uid = req.params.uid;
     
 });
