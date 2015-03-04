5728be8a9c524a5eb4437e3ee8ff893e459f3139
justincoon <justin.tyler.coon@gmail.com>
2015-03-04 00:11:50 -0500

    added bookmark and notification routes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app     = express();
 
-app.use('/user', require('./user'));
-app.use('/course', require('./course'));
+app.use('/user'              , require('./user'));
+app.use('/user/notficiation' , require('./notification'));
+app.use('/user/bookmark'     , require('./bookmark'));     
+app.use('/course'            , require('./course'));
 
 var server = app.listen(app.get('port'), function() {
     console.log('Listening on port ' + app.get('port'));
