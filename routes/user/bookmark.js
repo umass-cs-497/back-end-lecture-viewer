3ded31bcfe671d33fbe9786f02b637db498b9214
justincoon <justin.tyler.coon@gmail.com>
2015-03-04 11:50:31 -0500

    filled bookmark.js with empty api call routes to be filled

--- routes/user/bookmark.js
+++ routes/user/bookmark.js
@@ -1,5 +1,34 @@
 var express     = require('express');
 var router      = express.Router();
-var bidregex    = require('../../app').bidregex;
+
+//Create bookmark for current user
+router.post('/', function(req,res) {
+
+});
+
+//Delete specific bookmark
+router.delete('/:id', function(req,res) {
+    var bid = req.params.id
+
+});
+
+//Edit specific bookmark
+router.put('/:id', function(req,res) {
+    var bid = req.params.id
+    
+});
+
+//Get user's bookmarks for specific course
+router.get('/course/:id', function(req,res) {
+    var cid = req.params.id;
+
+});
+
+//Get user's bookmarks for specific lecture of a course
+router.get('/:cid/lecture/:lid', function(req,res) {
+    var cid = req.params.cid;
+    var lid = reg.params.lid;
+
+});
 
 module.exports = router;
