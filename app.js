044081bb0ceecf72f64986689dd9c6384a53b7d2
justincoon <justin.tyler.coon@gmail.com>
2015-03-04 11:59:16 -0500

    added roster to routes in app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use('/user/notficiation' , require('./routes/user/notification'));
 app.use('/user/bookmark'     , require('./routes/user/bookmark'));     
 
 app.use('/course'            , require('./routes/course/course'));
+app.use('/course/roster'     , require('./routes/course/roster'));
 
 var server = app.listen(app.get('port'), function() {
     console.log('Listening on port ' + app.get('port'));
