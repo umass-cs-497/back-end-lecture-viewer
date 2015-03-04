34839af617ee8bcc24cf6d41fa07d8e66a05c7f8
justincoon <justin.tyler.coon@gmail.com>
2015-03-04 13:33:30 -0500

    updated app.js so that ./bin/www is not necessary

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app     = express();
+var port    = process.env.PORT || 3000;
 
 app.use('/user'              , require('./routes/user/user'));
 app.use('/user/notficiation' , require('./routes/user/notification'));
@@ -8,9 +9,8 @@ app.use('/user/bookmark'     , require('./routes/user/bookmark'));
 app.use('/course'            , require('./routes/course/course'));
 app.use('/course/roster'     , require('./routes/course/roster'));
 
-var server = app.listen(app.get('port'), function() {
-    console.log('Listening on port ' + app.get('port'));
+var server = app.listen(port, function() {
+    console.log('Listening on port ' + port);
 });
-
 
 module.exports = app;
