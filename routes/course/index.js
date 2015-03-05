34301ee7302062d901bba91f9b1436f12af342ec
justincoon <justin.tyler.coon@gmail.com>
2015-03-05 12:08:13 -0500

    added lecture to course api

--- routes/course/index.js
+++ routes/course/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 //Add module routes
 require('./roster').setup(router);
+require('./lecture').setup(router);
 
 //Create a course
 router.post('/', function(req,res) {
@@ -19,7 +20,7 @@ router.get('/:course_id', function(req,res) {
 //Edit course
 router.put('/:course_id', function(req,res) {
     var course_id = req.params.course_id;
-    
+
 });
 
 //Delete course
