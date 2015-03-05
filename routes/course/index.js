664c0a8624d5fdec261ab1528bc75907cdb4a47a
justincoon <justin.tyler.coon@gmail.com>
2015-03-05 09:23:01 -0500

    added more comments

--- routes/course/index.js
+++ routes/course/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+//Import modules
 var roster = require('./roster');
 
+//Add module routes
 roster.setup(router);
 
 //Create a course
