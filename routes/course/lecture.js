c91e2d5df9c0ae0e04858de6a60585fb51f1e9f2
justincoon <justin.tyler.coon@gmail.com>
2015-03-05 12:04:09 -0500

    added lecture api

---
+++ routes/course/lecture.js
@@ -0,0 +1,31 @@
+//Lecture API
+module.exports = {
+    setup: function(router) {
+        //Add a lecture to a course
+        router.post('/:course_id/lecture', function(req,res) {
+            var course_id = req.params.course_id;
+
+        });
+
+        //Get a specific lecture
+        router.get('/:course_id/lecture/:lecture_id', function(req,res) {
+            var course_id = req.params.course_id;
+            var lecture_id = req.params.lecture_id;
+
+        });
+
+        //Edit a specific lecture
+        router.put('/:course_id/lecture/:lecture_id', function(req,res) {
+            var course_id = req.params.course_id;
+            var lecture_id = req.params.lecture_id;
+
+        });
+
+        //Delete a user from a course's roster
+        router.delete('/:course_id/lecture/:lecture_id', function(req,res) {
+            var course_id = req.params.course_id;
+            var lecture_id = req.params.lecture_id;
+
+        });
+    }
+};
