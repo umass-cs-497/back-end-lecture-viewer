e8aaf1ec0ec0394938a4889780925e35499fc429
justincoon <justin.tyler.coon@gmail.com>
2015-03-10 13:17:32 -0400

    completed /user/ test

--- test/user.js
+++ test/user.js
@@ -2,20 +2,19 @@ var should  = require('should');
 var assert  = require('assert');
 var request = require('supertest');
 
-var url = 'localhost:3000';
+var url = 'http://localhost:3000';
 
 describe('User', function() {
     describe('/ [POST]', function() {
         it('Should return success and user id', function(done) {
+            var body =  {email: 'me@me.me', 
+                         password: 'pw123', 
+                         first_name: 'Tim', 
+                         last_name: 'Richards'};
             request(url)
                 .post('/user')
-                .send({email: 'me@me.me', password: 'pw123', first_name: 'Tim', last_name: 'Richards'})
-                .end(function(err, res) {
-                    if(err)
-                        throw err;
-                    res.should.have.status(200);
-                    done();
-                });
+                .send(body)
+                .expect(200, done);
         });
     });
 });
