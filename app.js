e795b8ab5490b56a6cdb971aa5bec9b285882b40
justincoon <justin.tyler.coon@gmail.com>
2015-03-10 13:16:47 -0400

    added morgan for info on path usage

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var express = require('express');
-var app     = express();
-var port    = process.env.PORT || 3000;
+var express    = require('express');
+var devlog     = require('morgan');
+var bodyParser = require('body-parser');
+var app        = express();
+var port       = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(devlog('dev'));
 
 app.use('/user', require('./routes/user/index'));
 app.use('/course', require('./routes/course/index'));
