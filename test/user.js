a5ddfefa4ae76c88e00346ebe8ba2b33d9295dc2
justincoon <justin.tyler.coon@gmail.com>
2015-03-10 19:18:52 -0400

    added more tests

--- test/user.js
+++ test/user.js
@@ -5,16 +5,45 @@ var request = require('supertest');
 var url = 'http://localhost:3000';
 
 describe('User', function() {
+
+    //POST /user
     describe('/ [POST]', function() {
+
+        //Mock Data
+        var body =  {email: 'me@me.me', 
+                     password: 'pw123', 
+                     first_name: 'Tim', 
+                     last_name: 'Richards'};
+
         it('Should return success and user id', function(done) {
-            var body =  {email: 'me@me.me', 
-                         password: 'pw123', 
-                         first_name: 'Tim', 
-                         last_name: 'Richards'};
             request(url)
                 .post('/user')
                 .send(body)
                 .expect(200, done);
+        });
+        it('Should return failure: incorrect parameters', function(done) {
+            request(url)
+                .post('/user')
+                .send({email: body.email, password: body.password, first_name: body.first_name})
+                .end(function(err, res) {
+                    if(err) return done(err);
+                    res.body.status.should.equal('fail');
+                    done();
+                });
+        });
+    });
+
+    //GET /user
+    describe('/ [GET]', function() {
+        it('Should return an object with user info', function(done) {
+            request(url)
+                .get('/user')
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+                    res.body.data.should.have.properties('first_name', 'last_name', 'course_list');
+                    done();
+                });
         });
     });
 });
