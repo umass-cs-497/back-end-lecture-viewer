d08aa9b9d001dba5cd68c1b01521a48b9bb2e1cf
justincoon <justin.tyler.coon@gmail.com>
2015-03-10 00:42:12 -0400

    added base code for /user/ api calls, added mock data for user routes

--- routes/user/index.js
+++ routes/user/index.js
@@ -5,31 +5,80 @@ var router = express.Router();
 require('./bookmark').setup(router);
 require('./notification').setup(router);
 
+/*-----MOCK DATA-------*/
+var uid = '2394798792';
+var fname = 'Jane';
+var lname = 'Doe';
+var course_list = [{'name': 'CS497S Scalable Web Systems', 'id': '2348276591'}];
+var profile_picture = 'http://faculty.sites.uci.edu/ltemplate/files/2011/04/generic_profile.jpg';
+/*-----MOCK DATA-------*/
+
 //Create an account
 router.post('/', function(req,res) {
-
+    //Check if all required parameters are present
+    if(req.body.email && req.body.password && req.body.first_name && req.body.last_name) {
+        res.send({'status': 'success', 'data': {'user_id': uid}});
+    }
+    else {
+        res.send({'status': 'fail', 'data': {'title': 'Incorrect paramaters'}});
+    }
 });
 
 //Get logged in user info
 router.get('/', function(req,res) {
-
+    res.send({'status': 'success',
+        'data': {
+            'first_name': fname,
+            'last_name': lname,
+            'course_list': course_list
+        }
+    });
 });
 
 //Get user
 router.get('/:user_id', function(req,res) {
     var user_id = req.params.user_id;
-
+    res.send({'status': 'success',
+        'data': {
+            'first_name': fname,
+            'last_name': lname,
+            'profile_picture': profile_picture
+        }
+    });
 });
 
 //Edit user profile
 router.put('/:user_id', function(req,res) {
     var user_id = req.params.user_id;
 
+    if(req.body.fname) {
+        //Update user first name in db
+    }
+    if(req.body.lname) {
+        //Update user last name in db
+    }
+    if(req.body.profile_picture) {
+        //Update user profile pic in db
+    }
+
+    res.send({'status': 'success',
+        'data': {
+            
+        }
+    });
 });
 
 //Delete user
 router.delete('/:user_id', function(req,res) {
     var user_id = req.params.user_id;
+
+    //Delete user in database
+
+    res.send({'status': 'success',
+        'data': {
+            
+        }
+    });
 
 });
 
