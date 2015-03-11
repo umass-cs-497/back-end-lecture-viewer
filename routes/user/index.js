199c3eea1afea813c3ee11de2e1daf554ce1501f
justincoon <justin.tyler.coon@gmail.com>
2015-03-11 18:17:24 -0400

    created second delete for delete self in /user

--- routes/user/index.js
+++ routes/user/index.js
@@ -17,14 +17,14 @@ var mock_profile_picture = 'http://faculty.sites.uci.edu/ltemplate/files/2011/04
 router.post('/', function(req,res) {
     //Check if all required parameters are present
     if(req.body.email && req.body.password && req.body.first_name && req.body.last_name) {
-        res.status(200).send({'status': 'success', 
+        res.status(200).send({'status': 'success',
             'data': {
                 'user_id': mock_uid
             }
         });
     }
     else {
-        res.send({'status': 'fail', 
+        res.status(400).send({'status': 'fail',
             'data': {
                 'title': 'Incorrect paramaters'
             }
@@ -69,19 +69,32 @@ router.put('/:user_id', function(req,res) {
 
     res.status(200).send({'status': 'success',
         'data': {
-            
+
         }
     });
 });
 
-//Delete user
+//Delete current user
+router.delete('/', function(req,res) {
+
+    //Delete user in database
+
+    res.status(200).send({'status': 'success',
+        'data': {
+
+        }
+    });
+
+});
+
+//Delete a user
 router.delete('/:user_id', function(req,res) {
 
     //Delete user in database
 
     res.status(200).send({'status': 'success',
         'data': {
-            
+
         }
     });
 
