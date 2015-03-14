b47fe9cf2b48626a5d8982f596e09528c08a9c15
justincoon <justin.tyler.coon@gmail.com>
2015-03-13 20:33:22 -0400

    removed unnecessary dependencies

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 var express    = require('express');
 var devlog     = require('morgan');
 var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
 var app        = express();
 var port       = process.env.PORT || 3000;
-
-//Connect to mongodb on digitalocean droplet
-mongoose.connect('mongodb: //root:cs497s@104.131.30.37:27017');
-mongoose.connection.on('error', function() {
-    console.log('Error: Database not found');
-});
 
 //Tell node to interpret post data as JSON
 app.use(bodyParser.json());
