eddf537e753a299bf3ceea64cf007a1a9f3700de
Ryan Mullens <programthehacks@gmail.com>
2015-03-30 22:58:31 -0400

    Added file uploading for zipped lectures

--- routes/course/lecture.js
+++ routes/course/lecture.js
@@ -1,9 +1,70 @@
+var formidable = require('formidable')
+var fs = require('fs');
+var unzip = require('unzip');
+var uuid = require('node-uuid');
+
 //Lecture API
 module.exports = {
     setup: function(router) {
         //Add a lecture to a course
-        router.post('/:course_id/lecture', function(req,res) {
+        router.post('/:course_id/lecture', function(req,res) 
+        {
+            console.log("POST NEW LECTURE");
 
+            //TODO account for single, manual upload by Prof.
+
+
+            //Allows Formidable
+            var form = new formidable.IncomingForm();
+
+            /*Creates an event listener to print progress of upload to the console.
+                Plan on sending progress data back to client eventually.
+            */
+            form.addListener('progress' , function(bytesRecieved , bytesExpected){
+               
+              console.log(((bytesRecieved) + "/" + bytesExpected) + " : " + ((bytesRecieved * 100)/bytesExpected) + "%");
+            
+            });
+
+
+            form.parse(req, function(err, fields, files) 
+            {
+                //Gets the path that formidable placed file
+                var tempPath = files['upload']['path'];
+
+                console.log("Received upload. Placed in " + tempPath);
+
+                //Create a new random, unique, folder to save all unzipped lecture files
+                var unzipPath = "media/" + uuid.v1() + "/";
+
+                //Extracts files from temp zip file
+                fs.createReadStream(tempPath).pipe(unzip.Extract({ path: unzipPath }))
+                .on('error', function (error) {
+                    /*On unzipping error, abort request. Doesn't work for all file types, such as .txt*/
+                    console.log("File uploaded was not a .zip file. Aborting upload.");
+                    console.log(error);
+                    res.send("Invalid File Type");
+                    return;
+                })
+                .on('finish', function(end)
+                {   
+                    /*Once completing the unzipping, read other params and respond*/
+
+                    console.log("Unzipped files placed in " + unzipPath);      
+
+                    //How to recieve other fields from a multipart POST request
+                    var semester = fields['semester'];
+                    var course = fields['course'];
+                    var date = fields['date'];
+
+                    console.log(semester + " : " + course + " : " + date + " --- " + unzipPath);
+
+                    //Test response to prove it works
+                    res.write(semester + " : " + course + " : " + date + " --- " + unzipPath + "</br></br>");
+
+                    res.end();
+                });
+            });
         });
 
         //Get a specific lecture
