df8007d05743019178a1d5d729613e6dc4046821
Ryan Mullens <programthehacks@gmail.com>
2015-04-01 00:55:09 -0400

    Added functionality for adding single user to roster

--- routes/course/roster.js
+++ routes/course/roster.js
@@ -11,86 +11,114 @@ module.exports = {
 
         });
 
-        //Add a single(TODO) or a bunch of users to the course's roster
+        //Add a single or a bunch of users to the course's roster
         router.post('/:course_id/roster', function(req,res) {
 
-            //Creates form that can read in file
-            var form = new formidable.IncomingForm();
+            //Checks for email, if it does not exist, we assume a file is here
+            if(req.body.email == undefined)
+            {
+                //Creates form that can read in file
+                var form = new formidable.IncomingForm();
 
-            //Starts parsing of the file
-            form.parse(req, function(err, fields, files) {
+                //Starts parsing of the file
+                form.parse(req, function(err, fields, files) {
 
-                //Gets the temp path the file was uploaded to
-                var tempPath = files['upload']['path'];
+                    //Gets the temp path the file was uploaded to
+                    //TODO check against this.. files['upload'] could be undefined
+                    var tempPath = files['upload']['path'];
 
-                console.log("Received upload. Placed in " + tempPath);
+                    console.log("Received upload. Placed in " + tempPath);
 
-                //Starts reading of the uploaded file
-                fs.readFile(tempPath, 'utf8', function (err,data) {
-                    if (err) {
-                        return console.log(err);
-                    }
-
-
-                    //TODO
-                    //Check if we are adding non duplicates or doing a complete overwrite
-
-
-                    //Create arrays for all the valid/invalid emails
-                    var validEmails = [];
-                    var invalidEmails = [];
-
-                    //Create a new csv parser and write the file data into it
-                    var parser = csv.parse();
-                    parser.write(data);
-
-                    //Iterate through each csv entry
-                    while(email = parser.read())
-                    {   
-                        //For some reason the parser returns arrays of 1 item... So this makes it 1 item
-                        email = email[0];
-
-                        //Make sure the entry is actually an email and not just say, a name.
-                        if(validator.isEmail(email))
-                        {
-                            validEmails.push(email)
+                    //Starts reading of the uploaded file
+                    fs.readFile(tempPath, 'utf8', function (err,data) {
+                        if (err) {
+                            return console.log(err);
                         }
-                        else
-                        {
-                            invalidEmails.push(email);
+
+
+                        //TODO
+                        //Check if we are adding non duplicates or doing a complete overwrite
+
+
+                        //Create arrays for all the valid/invalid emails
+                        var validEmails = [];
+                        var invalidEmails = [];
+
+                        //Create a new csv parser and write the file data into it
+                        var parser = csv.parse();
+                        parser.write(data);
+
+                        //Iterate through each csv entry
+                        while(email = parser.read())
+                        {   
+                            //For some reason the parser returns arrays of 1 item... So this makes it 1 item
+                            email = email[0];
+
+                            //Make sure the entry is actually an email and not just say, a name.
+                            if(validator.isEmail(email))
+                            {
+                                validEmails.push(email)
+                            }
+                            else
+                            {
+                                invalidEmails.push(email);
+                            }
                         }
-                    }
 
-                    //TODO: For each vaild email, check against the current roster. Only add if not there.
-                    //Or just add if we are overwriting
+                        //TODO: For each vaild email, check against the current roster. Only add if not there.
+                        //Or just add if we are overwriting
 
-                    //For testing only. Just displays what email were valid/invalid
-                    res.write("<h2>Valid Emails</h2><br>");
-                    for(var i =0;i<validEmails.length;i++)
-                    {  
-                        var email = (i+1) + ". " + validEmails[i];
-                        res.write(email + "<br>");
-                    }
+                        //For testing only. Just displays what email were valid/invalid
+                        res.write("<h2>Valid Emails</h2><br>");
+                        for(var i =0;i<validEmails.length;i++)
+                        {  
+                            var email = (i+1) + ". " + validEmails[i];
+                            res.write(email + "<br>");
+                        }
 
-                    res.write("<br><br><br><h2>Invalid Emails</h2><br>");
-                    for(var i =0;i<invalidEmails.length;i++)
-                    {  
-                        var nonEmail = (i+1) + ". " + invalidEmails[i];
-                        res.write(nonEmail + "<br>");
-                    }
-
-
-                    res.write("<br><br>");
+                        res.write("<br><br><br><h2>Invalid Emails</h2><br>");
+                        for(var i =0;i<invalidEmails.length;i++)
+                        {  
+                            var nonEmail = (i+1) + ". " + invalidEmails[i];
+                            res.write(nonEmail + "<br>");
+                        }
 
 
-                    //This is more like what would be returned, a JSON object with lists of what was accepted
+                        res.write("<br><br>");
 
-                    responseObject = {"emails_added" : validEmails, "emails_rejected" : invalidEmails};
 
-                    res.write(JSON.stringify(responseObject));
-                    res.end();
-                });    
-            });
+                        //This is more like what would be returned, a JSON object with lists of what was accepted
+
+                        responseObject = {"emails_added" : validEmails, "emails_rejected" : invalidEmails};
+
+                        res.write(JSON.stringify(responseObject));
+                        res.end();
+                    });    
+                });
+            //We were given a json object of a single user
+            }else{
+                var email = req.body.email;
+
+                responseObject = {data:{}};
+
+                //Email provided is legit
+                if(validator.isEmail(email))
+                {
+                    //TODO add email to roster
+
+                    responseObject.status = "success";
+                    responseObject.data.email = email;
+                }
+                //Email provided is bad
+                else
+                {
+                    responseObject.status = "fail";
+                    responseObject.data.message = "Email provided is invalid";
+                    responseObject.data.email = email;
+                }
+
+                res.send(responseObject);
+            }
         });
 
         //Delete a user from a course's roster
