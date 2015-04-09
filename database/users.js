7cc6004a3319d22342d57f5867c19944b7b04887
Albert <albertung@gmail.com>
2015-04-09 14:34:08 -0400

    added user/course schemas

--- database/users.js
+++ database/users.js
@@ -3,26 +3,26 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // Schema definition for users
-var userSchema = new Schema({
-  email: {type: String, unique: true, lowercase: true},
-  password: String,
-  username: String,
-  name: {
-    first: String,
-    last: String
-  },
-  role: String,
-  // list of references to registered courses, element should be ObjectIds in Course collection.
-  courses: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Course'
-  }],
-  notifications: [],
-  bookmarks: [{
-    title: String,
-    url: String
-  }]
-});
+// var userSchema = new Schema({
+//   email: {type: String, unique: true, lowercase: true},
+//   password: String,
+//   username: String,
+//   name: {
+//     first: String,
+//     last: String
+//   },
+//   role: String,
+//   // list of references to registered courses, element should be ObjectIds in Course collection.
+//   courses: [{
+//     type: Schema.Types.ObjectId,
+//     ref: 'Course'
+//   }],
+//   notifications: [],
+//   bookmarks: [{
+//     title: String,
+//     url: String
+//   }]
+// });
 
 /*
   Useful methods to access the User database.
---
+++ models/course.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var courseSchema = new Schema({
+  courseNumber: String,
+  courseSection: String,
+  department: String,
+  description: String,
+  permission: Number,
+  instructors: [{
+    type: String,
+    unique: true
+  }],
+  // list of references to lectures, elements should be ObjectIds in Lecture collection.
+  lectures: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Lecture'
+  }],
+  // list of references to registered students/instructors, elements should be ObjectIds in User collection.
+  registeredUsers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  // list of all eligible emails to view the course's material (different from registeredUsers above.)
+  emails: [String],
+  semester: String,
+  title: String
+});
+
+module.exports = mongoose.model('Course', userSchema);
