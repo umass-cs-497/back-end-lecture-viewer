bddff2dd8878ebc380f18fa0a5a647ddf371f563
Albert <albertung@gmail.com>
2015-04-09 14:30:32 -0400

    Fixed database/index.js

--- database/index.js
+++ database/index.js
@@ -1,14 +1,17 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://freddy:freddy@ds043170.mongolab.com:43170/learn_u');
 
-var User = require('./users.js').User;
-var Course = require('./courses.js').Course;
-var Lecture = require('./lectures.js').Lecture;
-var Comment = require('./comments.js').Comment;
+// var User = require('./users.js').User;
+// var Course = require('./courses.js').Course;
+// var Lecture = require('./lectures.js').Lecture;
+// var Comment = require('./comments.js').Comment;
 
-module.exports = {
-  users: User,
-  courses: Course,
-  lectures: Lecture,
-  comments: Comment
-};
+// module.exports = {
+//   users: User,
+//   courses: Course,
+//   lectures: Lecture,
+//   comments: Comment
+// };
+
+exports.user = require('./user');
+exports.course = require('./course');
